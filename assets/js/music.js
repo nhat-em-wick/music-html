208f5eff8732fcc3ccbc59b1c7b73461c3bbd5de
nhat-em-wick <mnhat233@gmail.com>
2022-04-18 00:08:08 +0700

    UI music html

---
+++ assets/js/music.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 30,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
